fc118ee8581bb3f57c13fc1ec2a5863b19b3d034
Matias Gathoye <89335417+breyting@users.noreply.github.com>
2025-05-26 07:33:17 +0200

    learned the serveMux and handle

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github/breyting/http
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func main() {
+	serveMux := http.NewServeMux()
+
+	server := http.Server{
+		Handler: serveMux,
+		Addr:    ":8080",
+	}
+
+	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+
+	server.ListenAndServe()
+}
